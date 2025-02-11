6ca02549735506aba0559e0c94053712ce237633
Geoff Clements <geoff@electron.me.uk>
2025-02-11 19:09:38 +0000

    Allows dollar signs in text mode

--- math-mode.js
+++ math-mode.js
@@ -1,4 +1,5 @@
 [
+    { trigger: "\\$", replacement: "$", priority: 1, options: "tA" },
     { trigger: "$", replacement: "$$0$", options: "tA" },
     { trigger: "$", replacement: "$$0$", options: "mA" },
 ]
