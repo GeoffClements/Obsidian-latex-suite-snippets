748cb25e3d1528316c3c0ae6cd8cef69e28af4b8
Geoff Clements <geoff@electron.me.uk>
2025-02-11 19:49:04 +0000

    Improved square root

--- supsubscripts.js
+++ supsubscripts.js
@@ -6,7 +6,7 @@
     { trigger: /([^A-Za-z][A-Za-z])([A-Za-z])_/, replacement: "[[0]]_{[[1]]}", options: "rmA", priority: 1 },
     { trigger: "inv", replacement: "^{-1}", options: "mA" },
     { trigger: "stx", replacement: "_\\text{$0}", options: "mA" },
-    { trigger: "sqrt", replacement: "\\sqrt{$0}$1", options: "mA" },
+
     { trigger: "//", replacement: "\\frac{$0}{$1}$2", options: "mA" },
     { trigger: "ee", replacement: "e^{$0}$1", options: "mA" },
 
@@ -14,4 +14,14 @@
     { trigger: /\\hat{([A-Za-z])}(\d)/, replacement: "\\hat{[[0]]}_{[[1]]}", options: "rmA" },
     { trigger: /\\vec{([A-Za-z])}(\d)/, replacement: "\\vec{[[0]]}_{[[1]]}", options: "rmA" },
     { trigger: /\\mathbf{([A-Za-z])}(\d)/, replacement: "\\mathbf{[[0]]}_{[[1]]}", options: "rmA" },
+
+    {
+        trigger: "([2-9]?)sqrt", replacement: (match) => {
+            let order = match[1];
+
+            if (order != "") order = "[" + order + "]";
+            return "\\sqrt" + order + "{$0}";
+        },
+        options: "rmA"
+    },
 ]
