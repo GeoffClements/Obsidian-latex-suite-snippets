194d00604b870163fd5d91b65e4a9d7aeb0cc07b
Geoff Clements <geoff@electron.me.uk>
2025-03-17 11:09:39 +0000

    Addition of logic

---
+++ logic.js
@@ -0,0 +1,10 @@
+[
+    { trigger: "ng", replacement: "\\neg", options: "mA" },
+    { trigger: "nt", replacement: "\\lnot", options: "mA" },
+    { trigger: "land", replacement: "\\wedge", options: "mA" },
+    { trigger: "nand", replacement: "\\barwedge", options: "mA" },
+    { trigger: "lor", replacement: "\\vee", options: "mA" },
+    { trigger: "nor", replacement: "\\bar{\\vee}", options: "mA" },
+    { trigger: "xor", replacement: "\\barvee", options: "mA" },
+    { trigger: "xnor", replacement: "\\bar{\\veebar}", options: "mA" },
+]
