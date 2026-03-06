ad79ff6ac9f168f6c39311391a42eab2f10dea48
Geoff Clements <geoff@electron.me.uk>
2026-03-06 12:20:30 +0000

    Prevent clash between bracket norm and logic nor

--- brackets.js
+++ brackets.js
@@ -1,7 +1,7 @@
 [
     { trigger: "avg", replacement: "\\langle $0 \\rangle $1", options: "mA" },
-    { trigger: "norm", replacement: "\\lvert $0 \\rvert $1", options: "mA", priority: 1 },
-    { trigger: "Norm", replacement: "\\lVert $0 \\rVert $1", options: "mA", priority: 1 },
+    { trigger: "[Nn]rm", replacement: "\\lvert $0 \\rvert $1", options: "rmA", priority: 1 },
+    // { trigger: "Norm", replacement: "\\lVert $0 \\rVert $1", options: "mA", priority: 1 },
     { trigger: "ceil", replacement: "\\lceil $0 \\rceil $1", options: "mA" },
     { trigger: "floor", replacement: "\\lfloor $0 \\rfloor $1", options: "mA" },
     { trigger: "mod", replacement: "|$0|$1", options: "mA" },
